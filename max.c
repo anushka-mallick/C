14f0f643e015cd8605c229cbf0d2cd0c51038fc0
anushka-mallick <anushkamallick101@gmail.com>
2025-02-10 00:12:49 +0530

    Max number

---
+++ max.c
@@ -0,0 +1,14 @@
+// sample program - testing git push
+
+#include <stdio.h>
+
+int main() {
+    int num1 = 5, num2 = 10;
+
+    if (num1 > num2)
+        printf("num1 is greater");
+    else
+        printf("num2 is greater");
+
+    return 0;
+}
